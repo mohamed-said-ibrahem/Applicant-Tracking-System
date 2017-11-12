11dcfcab3300e6cc0c2ceab5e333b2008a57b7bf
mohamed said <ms.softxpert@gmail.com>
2017-11-12 09:29:48 +0200

    roles done

---
+++ src/WorkBundle/Controller/ApplicationController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace WorkBundle\Controller;
+
+use WorkBundle\Entity\Application;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Application controller.
+ *
+ */
+class ApplicationController extends Controller
+{
+    /**
+     * Lists all application entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $applications = $em->getRepository('WorkBundle:Application')->findAll();
+
+        return $this->render('application/index.html.twig', array(
+            'applications' => $applications,
+        ));
+    }
+
+    /**
+     * Creates a new application entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $application = new Application();
+        $form = $this->createForm('WorkBundle\Form\ApplicationType', $application);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($application);
+            $em->flush();
+
+            return $this->redirectToRoute('applicat_show', array('id' => $application->getId()));
+        }
+
+        return $this->render('application/new.html.twig', array(
+            'application' => $application,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a application entity.
+     *
+     */
+    public function showAction(Application $application)
+    {
+        $deleteForm = $this->createDeleteForm($application);
+
+        return $this->render('application/show.html.twig', array(
+            'application' => $application,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing application entity.
+     *
+     */
+    public function editAction(Request $request, Application $application)
+    {
+        $deleteForm = $this->createDeleteForm($application);
+        $editForm = $this->createForm('WorkBundle\Form\ApplicationType', $application);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('applicat_edit', array('id' => $application->getId()));
+        }
+
+        return $this->render('application/edit.html.twig', array(
+            'application' => $application,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a application entity.
+     *
+     */
+    public function deleteAction(Request $request, Application $application)
+    {
+        $form = $this->createDeleteForm($application);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($application);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('applicat_index');
+    }
+
+    /**
+     * Creates a form to delete a application entity.
+     *
+     * @param Application $application The application entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Application $application)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('applicat_delete', array('id' => $application->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/WorkBundle/Form/ApplicationType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WorkBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApplicationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('first_name')->add('middle_name')->add('last_name')->add('address')->add('email')->add('phone_number')->add('id_card_number')->add('date_available')->add('desired_salary')->add('hiring_way')->add('interviewed_before')->add('applied_position')->add('is_worked_before')->add('signature')->add('technical_test')->add('iq_test')->add('technical_comments')->add('technical_result')->add('personality_profile')->add('hr_notes')->add('gm_result')->add('final_decision')->add('created_at')->add('updated_at')->add('Education');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WorkBundle\Entity\Application'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'workbundle_application';
+    }
+
+
+}
