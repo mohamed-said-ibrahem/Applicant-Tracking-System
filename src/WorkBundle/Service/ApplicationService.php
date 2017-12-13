95bda6ddb8ea4456e5c76967ac387e353ac6a136
mohamed said <ms.softxpert@gmail.com>
2017-12-13 03:42:40 +0200

    change password btn

--- src/WorkBundle/Service/ApplicationService.php
+++ src/WorkBundle/Service/ApplicationService.php
@@ -5,6 +5,13 @@ namespace WorkBundle\Service;
 use Doctrine\ORM\EntityManager;
 use WorkBundle\Entity\Application;
 
+/**
+ * Application Service class the main service layer for the Applicants.
+ *
+ * This service class contains Application&Business logic. 
+ *
+ * @Author Mohamed said.
+ */ 
 class ApplicationService {
     
     private $em;
@@ -14,6 +21,37 @@ class ApplicationService {
         $this->em = $em;
     }
     
+    /**
+     * Creates new application for the applicant service.
+     *
+     * @param firstName         first name for the applicant.
+     * @param middleName        middle name for the applicant.
+     * @param lastName          last name for the applicant.
+     * @param address           address for the applicant.
+     * @param email             email for the applicant.
+     * @param phoneNumber       phone number for the applicant.
+     * @param idCardNumber      id card number for the applicant.
+     * @param dateAvailable     date available for applicant to join.
+     * @param desiredSalary     desired salary  for the applicant.
+     * @param hiringWay         hiring way for the applicant.
+     * @param interviewedBefore if applicant is interviewd before or not?
+     * @param appliedPosition   apllied position for the applicant.
+     * @param isWorkedBefore    if applicant worked before?
+     * @param signature         applicant signature.
+     * @param schoolName        apllicant school name.
+     * @param schoolFrom        applicant start school date.
+     * @param schoolTo          applicant end school date.
+     * @param collegeName       apllicant college name.
+     * @param collegeFrom       applicant college start date.
+     * @param collegeTo         apllicant college end date.
+     * @param isGraduated       does applicant graduate?
+     * @param graduateDegree    applicant graduated degree. 
+     * 
+     * @throws Some_Exception_Class  there is not exeptions
+     * @author Mohamed Said
+     * 
+     * There is no return type for this method as it creates a new applicant only. 
+     */ 
     public function createNewApplicant($firstName,$middleName,$lastName,
     $address,$email,$phoneNumber,$idCardNumber,$dateAvailable,$desiredSalary,
     $hiringWay,$interviewedBefore,$appliedPosition,$isWorkedBefore,$signature,
@@ -29,7 +67,7 @@ class ApplicationService {
                 ->createEducation($employee,$schoolName,$schoolFrom,$schoolTo,
                 $schoolDegree,$collegeName,$collegeFrom,$collageTo,$isGraduated,$graduateDegree);
                 
-        return $education;
+        // return $education;
     }
 
     public function findApplicantById($id)
@@ -41,7 +79,7 @@ class ApplicationService {
     public function findApplicantByNameOrEmail($input)
     {
         return $this->em->getRepository('WorkBundle:Application')
-                ->findByUserOrEmail($input);
+                ->findByNameOrEmail($input);
     }
     
     public function findApplicantByPhone($phone)
